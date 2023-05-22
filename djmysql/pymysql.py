b64d7891972f952e9d87142c669e0d37040000e5
Shreeshail Mahajan <85840341+im-shree09@users.noreply.github.com>
2023-05-23 00:31:31 +0530

    Update pymysql.py

--- djmysql/pymysql.py
+++ djmysql/pymysql.py
@@ -1,3 +1,5 @@
+# CREATE USER raj@localhost IDENTIFIED BY 'raj123';
+# GRANT ALL PRIVILEGES ON newdb.* TO raj@localhost;
 import mysql.connector
 conn=mysql.connector.connect(host='localhost', password='shree@123', user='shree')
 
